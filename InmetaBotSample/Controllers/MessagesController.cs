ce5a44cdb57f36b320ef260cd0f22173c85d068c
Mário Henriques <mariovaz.henriques@inmeta.no>
2016-08-02 19:10:01 +0200

    added first files

--- InmetaBotSample/Controllers/MessagesController.cs
+++ InmetaBotSample/Controllers/MessagesController.cs
@@ -1,69 +1,64 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Connector.Utilities;
-using Newtonsoft.Json;
+using System.Web.Services.Description;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System.Web.Http.Description;
+using System.Net.Http;
+using System.Diagnostics;
+using System;
+using System.Configuration;
 
 namespace InmetaBotSample
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+
+            internal static IDialog<SandwichOrder> MakeRootDialog()
+            {
+                return Chain.From(() => FormDialog.FromForm(SandwichOrder.BuildForm));
+            }
+
+
         /// <summary>
         /// POST: api/Messages
-        /// Receive a message from a user and reply to it
+        /// receive a message from a user and send replies
         /// </summary>
-        public async Task<Message> Post([FromBody]Message message)
+        /// <param name="activity"></param>
+        [ResponseType(typeof(void))]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            if (message.Type == "Message")
+            ConfigurationManager.AppSettings["BotId"] = "test";
+            try
             {
-                // calculate something for us to return
-                int length = (message.Text ?? string.Empty).Length;
+                if (activity != null)
+                {
+                    // one of these will have an interface and process it
+                    switch (activity.GetActivityType())
+                    {
+                        case ActivityTypes.Message:
+                            await Conversation.SendAsync(activity, MakeRootDialog);
+                            break;
 
-                // return our reply to the user
-                return message.CreateReplyMessage($"You sent {length} characters");
-            }
-            else
-            {
-                return HandleSystemMessage(message);
-            }
-        }
+                        case ActivityTypes.ConversationUpdate:
+                        case ActivityTypes.ContactRelationUpdate:
+                        case ActivityTypes.Typing:
+                        case ActivityTypes.DeleteUserData:
+                        default:
+                            Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                            break;
+                    }
+                }
+                return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
 
-        private Message HandleSystemMessage(Message message)
-        {
-            if (message.Type == "Ping")
-            {
-                Message reply = message.CreateReplyMessage();
-                reply.Type = "Ping";
-                return reply;
             }
-            else if (message.Type == "DeleteUserData")
+            catch (Exception exp)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
+                var error = exp.Message;
+                return null;
             }
-            else if (message.Type == "BotAddedToConversation")
-            {
-            }
-            else if (message.Type == "BotRemovedFromConversation")
-            {
-            }
-            else if (message.Type == "UserAddedToConversation")
-            {
-            }
-            else if (message.Type == "UserRemovedFromConversation")
-            {
-            }
-            else if (message.Type == "EndOfConversation")
-            {
-            }
-
-            return null;
         }
     }
 }
---
+++ InmetaBotSample/CustomConfigurationReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InmetaBotSample
+{
+    public class CustomConfigurationReader
+    {
+        private string m_sConfigFileName = @"C:\Users\marhenri\Documents\InmetaBotSample\InmetaBotSample\DataSample.xml";
+        private CustomConfigurationReader m_oConfig = new CustomConfigurationReader();
+
+        public CustomConfigurationReader Config
+        {
+            get { return m_oConfig; }
+            set { m_oConfig = value; }
+        }
+
+        // Load configfile
+        public void LoadConfig()
+        {
+            if (System.IO.File.Exists(m_sConfigFileName))
+            {
+                System.IO.StreamReader srReader = System.IO.File.OpenText(m_sConfigFileName);
+                Type tType = m_oConfig.GetType();
+                System.Xml.Serialization.XmlSerializer xsSerializer = new System.Xml.Serialization.XmlSerializer(tType);
+                object oData = xsSerializer.Deserialize(srReader);
+                m_oConfig = (CustomConfigurationReader)oData;
+                srReader.Close();
+            }
+        }
+
+        // Save configfile
+        public void SaveConfig()
+        {
+            System.IO.StreamWriter swWriter = System.IO.File.CreateText(m_sConfigFileName);
+            Type tType = m_oConfig.GetType();
+            if (tType.IsSerializable)
+            {
+                System.Xml.Serialization.XmlSerializer xsSerializer = new System.Xml.Serialization.XmlSerializer(tType);
+                xsSerializer.Serialize(swWriter, m_oConfig);
+                swWriter.Close();
+            }
+        }
+    }
+
+
+}
